81be365b1d08429de8828a612d7f8ae5966f907b
Lukasz Sygula <lukasz.sygula@onet.pl>
2020-03-29 12:55:51 +0000

    Dodano funkcję tworzenia zadań

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const tasks = require('./routes/tasks')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -35,6 +36,7 @@ app.use(async (ctx, next) => {
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+app.use(tasks.routes(), tasks.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
